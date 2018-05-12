c3acd162f4297048a5c0bd8607c1d1cb071e9185
recon <recongamingsam@gmail.com>
2018-05-12 19:19:01 +1000

    Rewrote the examples

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,10 @@
-from disocrdWebhooks import Webhook
+from discord_hooks import Webhook
+WEBHOOK_URL = 'ADD YOUR WEBHOOK URL HERE'
 
-url = open('url').read()
-
-embed = Webhook(url, color=1234123, msg='this `supports` __a__ **subset** *of* ~~markdown~~ \U0001f603')
-
+embed = Webhook(
+    url=WEBHOOK_URL,
+    username='Hello World!',
+    color=1234123)
 embed.set_author(name='Author Goes Here', icon='https://i.imgur.com/rdm3W9t.png', url='https://discordapp.com/')
 embed.set_title(title='title ~~(did you know you can have markdown here too?)~~', url='https://discordapp.com/')
 embed.set_desc("this description supports [named links](https://discordapp.com) as well. ```\nyes, even code blocks```")
@@ -17,7 +18,3 @@ embed.set_image('https://cdn.discordapp.com/embed/avatars/0.png')
 embed.set_footer(text='Time Stamp is here =>',ts=True,icon='https://cdn.discordapp.com/embed/avatars/0.png')
 
 embed.post()
-
-
-
-
--- examples/simple_message.py
+++
@@ -1,8 +0,0 @@
-from discordWebhooks import Webhook
-
-
-url = open('url').read()
-
-msg = Webhook(url,msg="Hello there! I'm a webhook \U0001f62e")
-
-msg.post()
