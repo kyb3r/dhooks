370575ef30aaed8f0563a7f11f10119ffdd21550
recon <recongamingsam@gmail.com>
2018-05-12 19:19:41 +1000

    Fixed small color error in example.py

--- examples/example.py
+++ examples/example.py
@@ -4,7 +4,7 @@ WEBHOOK_URL = 'ADD YOUR WEBHOOK URL HERE'
 embed = Webhook(
     url=WEBHOOK_URL,
     username='Hello World!',
-    color=1234123)
+    color=123123)
 embed.set_author(name='Author Goes Here', icon='https://i.imgur.com/rdm3W9t.png', url='https://discordapp.com/')
 embed.set_title(title='title ~~(did you know you can have markdown here too?)~~', url='https://discordapp.com/')
 embed.set_desc("this description supports [named links](https://discordapp.com) as well. ```\nyes, even code blocks```")
