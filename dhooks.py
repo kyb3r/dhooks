bb6d5930caa751ed090ddd8d2c2c1888d60e79fe
verixx <abdurraqeeb53@gmail.com>
2018-11-12 00:37:02 +1100

    Pretty code

--- dhooks.py
+++ dhooks.py
@@ -1,5 +1,3 @@
-import asyncio
-import traceback
 import datetime
 
 import aiohttp
@@ -10,15 +8,16 @@ try:
 except ImportError:
     import json
 
+
 class Embed:
     '''Class that represents a discord embed'''
 
     __slots__ = (
-        'color','title','url','author',
-        'description','fields','image',
-        'thumbnail','footer','timestamp',
+        'color', 'title', 'url', 'author',
+        'description', 'fields', 'image',
+        'thumbnail', 'footer', 'timestamp',
         )
-    
+
     def __init__(self, **kwargs):
         '''Initialises an Embed object'''
         self.color = kwargs.get('color')
@@ -28,57 +27,57 @@ class Embed:
         self.fields = kwargs.get('fields', [])
 
         timestamp = kwargs.get('timestamp')
-        if timestamp is True: # sets the timestamp to the current time
+        if timestamp is True:  # sets the timestamp to the current time
             self.timestamp = str(datetime.datetime.utcnow())
         else:
             self.timestamp = timestamp
-          
+
     def del_field(self, index: int):
         '''Deletes a field by index'''
         self.fields.pop(index)
 
-    def set_title(self, title: str, url:str):
+    def set_title(self, title: str, url: str):
         self.title = title
         self.url = url
 
-    def set_timestamp(self, time: str=None, now: bool=False):
+    def set_timestamp(self, time: str = None, now: bool = False):
         if now:
             self.timestamp = str(datetime.datetime.utcnow())
         else:
             self.timestamp = str(time)
-      
-    def add_field(self, name: str, value: str, inline: bool=True):
+
+    def add_field(self, name: str, value: str, inline: bool = True):
         '''Adds a field'''
         field = {
-            'name': name, 
-            'value': value, 
+            'name': name,
+            'value': value,
             'inline': inline
             }
         self.fields.append(field)
-    
-    def set_author(self, name: str, icon_url: str=None, url: str=None):
+
+    def set_author(self, name: str, icon_url: str = None, url: str = None):
         '''Sets the author of the embed'''
         self.author = {
             'name': name,
-            'icon_url' : icon_url,
-            'url' : url
+            'icon_url': icon_url,
+            'url': url
             }
-    
+
     def set_thumbnail(self, url: str):
         '''Sets the thumbnail of the embed'''
         self.thumbnail = {'url': url}
-    
+
     def set_image(self, url):
         '''Sets the image of the embed'''
         self.image = {'url': url}
-        
-    def set_footer(self, text: str, icon_url: str=None):
+
+    def set_footer(self, text: str, icon_url: str = None):
         '''Sets the footer of the embed'''
         self.footer = {
             'text': text,
             'icon_url': icon_url
             }
-    
+
     def to_dict(self) -> dict:
         '''Turns the object into a dictionary'''
         return {
@@ -87,21 +86,22 @@ class Embed:
             if hasattr(self, key) and getattr(self, key)
             }
 
+
 class Webhook:
     '''Asynchronous client that makes it easy to use webhooks'''
-    
-    def __init__(self, url: str, session=None, is_async: bool=False, **options):
+
+    def __init__(self, url: str, session=None, is_async: bool = False, **options):
         self.url = url
         self.is_async = is_async
         self.session = session or (aiohttp.ClientSession() if is_async else requests.Session())
         self.headers = {'Content-Type': 'application/json'}
         self.username = options.get('username')
         self.avatar_url = options.get('avatar_url')
-    
+
     def close(self):
         self.session.close()
 
-    def send(self, content: str=None, embeds: list or Embed=[], tts: bool=False) -> bool:
+    def send(self, content: str = None, embeds: list or Embed = [], tts: bool = False) -> bool:
         '''Sends a message to the payload url'''
 
         payload = {
@@ -111,10 +111,10 @@ class Webhook:
             'tts': tts
             }
 
-        if not hasattr(embeds, '__iter__'): # supports a list/tuple of embeds 
-            embeds = [embeds]               # or a single embed
+        if not hasattr(embeds, '__iter__'):  # supports a list/tuple of embeds or a single embed
+            embeds = [embeds]
 
-        payload['embeds'] = [em.to_dict() for em in embeds] 
+        payload['embeds'] = [em.to_dict() for em in embeds]
 
         payload = json.dumps(payload, indent=4)
 
