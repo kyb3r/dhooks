7fe53103182f0da889a67575043133d63d39972e
Taku 3 Animals <45324516+Taaku18@users.noreply.github.com>
2019-12-05 22:20:09 -0800

    increment version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='dhooks',
     author='kyb3r',
     packages=find_packages(),
-    version='1.1.0',
+    version='1.1.1',
     description='An (a)sync wrapper for discord webhooks',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
@@ -32,7 +32,7 @@ setup(
             'sanic'
         }
     },
-    python_requires='>=3.5',
+    python_requires='>=3.5.3',
     url='https://github.com/4rqm/dhooks/',
     project_urls={
         'Issue Tracker': 'https://github.com/4rqm/dhooks/issues',
